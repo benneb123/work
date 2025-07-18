041c1b5cf8747ff5e4f0135af98e3f37c06ac38d
benneb123 <xgc1765633895@qq.com>
2025-07-18 11:06:29 +0800

    \首次提交\

--- app.py
+++ app.py
@@ -7,6 +7,11 @@ def index():
     print("index")
     return render_template('index.html')
 
+@app.route('/snake')
+def snake():
+    print("snake")
+    return render_template('snake.html')
+
 if __name__ == '__main__':
     print("main")
     app.run(debug=True) 
